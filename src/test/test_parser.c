6ffb2080bcf713421721111f4e39449bfbbcf7b1
MisakaCenter <misaka@sjtu.edu.cn>
2021-08-07 01:45:14 +0800

    feat(parser): assignment, break, continue, return.

--- src/test/test_parser.c
+++ src/test/test_parser.c
@@ -4,7 +4,11 @@
 //
 // test_parser.c for Simple-XX/SimpleCompiler.
 
-// expression
-!(11 + 45) - 14 * 19 / 19 % 810 + simple(1 + 1, 2) + xx(id(), 2) + comp[1][id(2)] + iler
-
-// Decl
+// statement
+!(11 + 45) - 14 * 19 / 19 % 810 + simple(1 + 1, 2) + xx(id(), 2) + comp[1][id(2)] + iler;
+simple = 1;
+simple[xx][compiler] = simple + 1;
+break;
+continue;
+return;
+return simple + xx;
